ee17a42c9a625bc80f3ef4277ba8807835c35b25
Daniel Piliutsin <102699155+DanielPiliu@users.noreply.github.com>
2023-02-04 21:00:32 -0500

    Update ubuntok.js

--- ubuntok.js
+++ ubuntok.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         UnbunTok
+// @name         UbunTok
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Makes TikTok less addicting by removing recommended videos and customizes the main pages!
@@ -9,21 +9,7 @@
 // ==/UserScript==
 
 (function() {
-  // Set the background image and color of the top bar
   document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1055867133239754762/1055867173253423125/2587485.png')";
-  let topBar = document.querySelector("div.tiktok-xk7ai4-DivHeaderContainer");
-// ==UserScript==
-// @name         Debtok
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Makes TikTok less addicting by removing recommended videos and customizes the main pages!
-// @author       Github.com/danielpiliu
-// @match        https://www.tiktok.com/*
-// @grant        none
-// ==/UserScript==
-
-(function() {
-  document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1055995168144236605/1071601580085891072/debiscoolpt2.webp')";
   let topBar = document.querySelector("div.tiktok-xk7ai4-DivHeaderContainer");
   topBar.style.backgroundColor = "#E95420";
 
