179612bce1d068c9d1a1847ef4b80a61502d0baa
Daniel Piliutsin <102699155+DanielPiliu@users.noreply.github.com>
2023-02-08 19:27:53 -0500

    Add files via upload

--- debtok.js
+++ debtok.js
@@ -9,7 +9,11 @@
 // ==/UserScript==
 
 (function() {
-  document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1055995168144236605/1071601580085891072/debiscoolpt2.webp')";
+   document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1055995168144236605/1071601580085891072/debiscoolpt2.webp')";
+   document.body.style.backgroundRepeat = "no-repeat";
+   document.body.style.backgroundSize = "100% 100%";
+   document.body.style.backgroundAttachment = "fixed";
+
   let topBar = document.querySelector("div.tiktok-xk7ai4-DivHeaderContainer");
   topBar.style.backgroundColor = "#7DB6F0";
 
@@ -17,7 +21,10 @@
     "div.tiktok-1id9666-DivMainContainer.ec6jhlz0",
     "div.tiktok-1a4urrd-DivWrapper.e14l9ebt2",
     "div.e14l9ebt0.tiktok-1hi889j-DivScrollContainer-StyledScroll.e1b4u1n1",
-    "div.e14l9ebt0.tiktok-176uj12-DivScrollContainer-StyledScroll.e1b4u1n1"
+    "div.e14l9ebt0.tiktok-176uj12-DivScrollContainer-StyledScroll.e1b4u1n1",
+    "div.tiktok-1qpva5c-DivListContainer.e1ymawm00",
+    "div.tiktok-1vrbodl-DivPromotionContainer.ejkrawn0",
+    "div.tiktok-1ps1qld-ButtonBasicButtonContainer-StyledVideoSwitch.e11s2kul14"
   ];
 
   let targetsColor = [
@@ -25,26 +32,35 @@
     "div.tiktok-3q30id-DivContentContainer.e1mecfx00",
     "div.tiktok-cy378a-DivCommentListContainer.ekjxngi0",
     "div.tiktok-19hqadz-DivBottomCommentContainer.e1mecfx04",
-    "div.tiktok-1htic8b-DivCommentListContainer.ekjxngi0"
+    "div.tiktok-1htic8b-DivCommentListContainer.ekjxngi0",
+    "div.tiktok-xlp5i2-PVideoListTitle.ege8lhx6"
   ];
 
-  function removeElements() {
-    targets.forEach(target => {
-      let element = document.querySelector(target);
-      if (element) {
-        element.parentNode.removeChild(element);
-      }
-    });
-
-    targetsColor.forEach(targetColor => {
-      let element = document.querySelector(targetColor);
-      if (element) {
-        element.style.backgroundColor = "#7DB6F0";
-      }
-    });
-
-    setTimeout(removeElements, 1000);
+function removeElements() {
+  let errorMessage = document.querySelector("div.tiktok-1tttox1-DivErrorContainer.emuynwa0");
+  if (errorMessage) {
+    errorMessage.parentNode.removeChild(errorMessage);
+    location.reload();
+    return;
   }
 
-  removeElements();
+  targets.forEach(target => {
+    let element = document.querySelector(target);
+    if (element) {
+      element.parentNode.removeChild(element);
+    }
+  });
+
+  targetsColor.forEach(targetColor => {
+    let element = document.querySelector(targetColor);
+    if (element) {
+      element.style.backgroundColor = "#7DB6F0";
+    }
+  });
+
+  setTimeout(removeElements, 1000);
+}
+
+
+removeElements();
 })();
